57552a9529dc7f5be5da2738ec807343a3f42fc3
jbowkley4 <jacques.bowkley@st-annes.ox.ac.uk>
2024-05-20 16:55:35 +0100

    A complete solution to Ex2

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,18 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
-bins=100 #adjust the number of bins to your plot
+filename="processed_iperf_test3.txt"
+label1='iperf1 s to c'
+label2='iperf2 c to s'
+xlabel = 'interval'
+ylabel = 'Bandwidth in Mbits/s'
+title='RP as server LM as client bi-directional t=10 i=1'
+fig_name='iperf_3_graph'
+bins=5000 #adjust the number of bins to your plot
 
 
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
 
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+i=t[:,0]
+l=t[:,1]
+
+plt.plot(i[::2], l[::2], label=label1)# Plot some data on the (implicit) axes.
+plt.plot(i[::2], l[1::2], label=label2)
 #Comment the line above and uncomment the line below to plot a CDF
 #plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
